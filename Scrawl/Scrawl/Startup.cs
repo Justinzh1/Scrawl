0671202a4080828679a34396e713c8fd49dfa818
connerwgillette <connerwgillette@gmail.com>
2015-06-19 13:30:27 -0700

    initial project creation

---
+++ Scrawl/Scrawl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scrawl.Startup))]
+namespace Scrawl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
