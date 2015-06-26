9fa233e923b1c715a17ab364f41500e3510ed16d
connerwgillette <connerwgillette@gmail.com>
2015-06-25 21:51:37 -0700

    layout changes + home page placeholder

--- Scrawl/Scrawl/App_Start/RouteConfig.cs
+++ Scrawl/Scrawl/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace Scrawl
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Dashboard",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
